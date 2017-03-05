a894531a7ca6f123d43ce985838f183cd77c6ac8
Heeseok Chong <heeseok.chong@gmail.com>
2017-03-06 04:29:29 +0900

    Added #ifdef for Windows dependency

--- DS01/main.cpp
+++ DS01/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#ifdef _WIN32
 #include <cstdlib>
+#endif
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
-	system("pause");
-
+#ifdef _WIN32
+    system("pause");
+#endif
     return 0;
 }
