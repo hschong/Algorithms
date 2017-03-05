cddb82b6e9caa70c128782cfffac2e284339fd84
Heeseok Chong <heeseok.chong@gmail.com>
2017-03-06 04:16:11 +0900

    "pause" was replaced with "read" and removed <cstdlib>.

--- DS01/main.cpp
+++ DS01/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <cstdlib>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
-	system("pause");
+	system("read ");
     return 0;
 }
