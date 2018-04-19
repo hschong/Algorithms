eb41b282b7dcb48f9fdd120acd9d39e1655d781b
Heeseok Chong <heeseok.chong@gmail.com>
2018-04-20 04:10:28 +0900

    Be carefull "    while min_idx <= max_idx:"

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,1 +1,22 @@
-def binary_search(list, item):    min_idx = 0    max_idx = len(list) - 1    while min_idx <= max_idx:        mid_idx = (min_idx + max_idx) // 2        value = list[mid_idx] # value[mid_idx]        if item == value:            return value        if item > value:            min_idx = mid_idx + 1        else:            max_idx = mid_idx - 1    return Nonemy_list = [1, 3, 5, 7, 9]print(binary_search(my_list, 3))
+def binary_search(list, item):
+    min_idx = 0
+    max_idx = len(list) - 1
+
+    while min_idx <= max_idx:
+
+        mid_idx = (min_idx + max_idx) // 2
+        guess = list[mid_idx]
+
+        if guess == item:
+            return guess
+        elif guess > item:
+            max_idx = mid_idx - 1
+        else:
+            min_idx = mid_idx + 1
+
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+print(binary_search(my_list, 3))
+
+
