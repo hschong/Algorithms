c7677b506b294affd19b2338c302898261d4593f
Heeseok Chong <heeseok.chong@gmail.com>
2019-06-01 20:23:51 +0900

    insert balls by list.

---
+++ insertBall.py
@@ -0,0 +1,59 @@
+class ListPipe:
+    def __init__(self):
+        self.pipe = []
+    
+    def addLeft(self, number):
+        self.pipe = [number] + self.pipe 
+        # 1+2+3+4+5+6+ ... + n = n(n+1)/2
+
+    def addRight(self, number):
+        self.pipe.append(number)
+
+    def getBeads(self):
+        return self.pipe
+
+
+def processBeads(myInput):
+    # myInput[i][0] : Elements' index
+    # myInput[i][1] : Direction to add, 0 : left, 1 : right
+
+    # myInput[0][0] = 1, myInput[0][1] = 0,
+    # myInput[1][0] = 2, myInput[1][1] = 1,
+    # myInput[2][0] = 3, myInput[2][1] = 0
+
+    # myInput[0] = ball = [1, 0]
+    # myInput[1] = ball = [2, 1]
+    # myInput[2] = ball = [3, 0]
+    # [[1,0], [2,1], [3,0]]
+
+    myPipe = ListPipe()
+
+    for ball in myInput:
+        if ball[1] == 0:
+            myPipe.addLeft(ball[0])
+        else:
+            myPipe.addRight(ball[0])
+
+    return myPipe.getBeads()
+
+
+def main():
+    n = int(input())
+    myList = []
+
+    for i in range(n) :
+        myList.append([int(v) for v in input().split()])
+
+    print(*processBeads(myList))
+
+if __name__ == "__main__":
+    main()
+
+# Input sample
+# 3
+# 1 0
+# 2 1
+# 3 0
+
+# Output sample
+# 3 1 2
--- insertBallsLinkedList.py
+++ insertBallsLinkedList.py
@@ -1,40 +1,3 @@
-# class ListPipe:
-#     def __init__(self):
-#         self.pipe = []
-    
-#     def addLeft(self, number):
-#         self.pipe = [number] + self.pipe 
-#         # 1+2+3+4+5+6+ ... + n = n(n+1)/2
-
-#     def addRight(self, number):
-#         self.pipe.append(number)
-
-#     def getBeads(self):
-#         return self.pipe
-
-#     def processBeads(self, myInput):
-#         # myInput[i][0] : i번째 구슬의 번호
-#         # myInput[i][1] : i번째에 넣는 방향
-        
-#         # myInput[0][0] = 1, myInput[0][1] = 0,
-#         # myInput[1][0] = 2, myInput[1][1] = 1,
-#         # myInput[2][0] = 3, myInput[2][1] = 0
-
-#         # myInput[0] = [1, 0]
-#         # myInput[1] = [2, 1]
-#         # myInput[2] = [3, 0]
-
-#         myPipe = ListPipe()
-
-#         for ball in myInput:
-#             if ball[1] == 0:
-#                 myPipe.addLeft(ball[0])
-#             else:
-#                 myPipe.addRight(ball[0])
-
-#         return myPipe.getBeads()
-
-
 class LinkedListElement:
     def __init__(self):
         self.value = None
@@ -80,9 +43,6 @@ class LinkedListPipe:
 
 
 def processBeads(myInput):
-    myPipe = LinkedListPipe()
-    result = []
-    
     # myInput[i][0] : Elements' index
     # myInput[i][1] : Direction to add, 0 : left, 1 : right
 
@@ -95,6 +55,9 @@ def processBeads(myInput):
     # myInput[2] = ball = [3, 0]
     # [[1,0], [2,1], [3,0]]
 
+    myPipe = LinkedListPipe()
+    result = []
+    
     for ball in myInput:
         if ball[1] == 0:
             myPipe.addLeft(ball[0])
