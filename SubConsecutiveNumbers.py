ecdb060efdfc6d70a4caa260369ea08f1b295562
Heeseok <heeseok.chong@gmail.com>
2019-08-01 23:21:44 +0900

    get the max sum of sub consecutive numbers.

---
+++ SubConsecutiveNumbers.py
@@ -0,0 +1,15 @@
+# Find the maximum sum of sub consecutive numbers in the list.
+def getMaxSum(numbers) :
+    maxSum = -9078942340913534
+    length = len(numbers)
+
+    for start in range(0, length) :
+        for end in range(start, length) :
+            tempSum = 0
+
+            for i in range(start, end+1) :
+                tempSum += numbers[i]
+            
+            maxSum = max(maxSum, tempSum)
+    
+    return maxSum
