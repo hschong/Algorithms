f7b840bc234415905f4fa5f63ada61d73d5a9879
Heeseok <heeseok.chong@gmail.com>
2019-08-02 20:39:36 +0900

    let the getPowerSet() clear by adding comments.

--- PowerSet.py
+++ PowerSet.py
@@ -4,12 +4,46 @@ import sys
 
 # Elements in the set are natural numbers and 
 # the numbers starts from 1 to n.
-# the n is the number of the elements in the set A. 
+# the n is the number of the elements in the set S. 
 
 # For instance, n = 3
-# set A = {1, 2, 3}
+# set S = {1, 2, 3}
 # power set of set A is { {}, {1}, {2}, {3}, 
 # {1, 2}, {1, 3}, {2, 3}, {1, 2, 3} }
+
+# getPowerSet
+# the first element of the each subset 
+# in the power set should start with element k.
+# ex) n = 3, k = 2
+#     [1, 2, 3] == set A
+#     [[2], [2, 3]] == getPowerSet(n, k)
+
+def getPowerSet(n, k) :
+    if n == k :
+        return [ [k] ]
+    else :
+        nth_after_k = [ [k] ] # nothing after k
+        sth_after_k = [] # something after k
+        
+        # getPowerSet(3, 1)
+        # nth_after_k = [ [1] ]
+        # sth_after_k = [ [2], [2, 3], [3] ]
+        # nth_after_k + sth_after_k == 
+        #   [ [1], [1, 2, ...], [1, 3, ...] ] 
+        
+        # getPowerSet(3, 2) == [[2], [2, 3]]
+        # getPowerSet(3, 3) == [[3]]
+        # getPowerSet(3, 2) + getPowerSet(3, 3) 
+        #   == [[2], [2, 3], [3]]
+
+        for i in range(k+1, n+1) :
+            sth_after_k = sth_after_k + getPowerSet(n, i)
+    
+        for i in range(len(sth_after_k)) :
+            sth_after_k[i] = [k] + sth_after_k[i]
+            
+        return nth_after_k + sth_after_k
+
 
 def powerSet(n) :
     '''
@@ -26,30 +60,6 @@ def powerSet(n) :
     
     return result
 
-
-# the first element of the each subset 
-# in the power set should start with element k.
-# ex) n = 3, k = 2
-#     [1, 2, 3] == set A
-#     [[2], [2, 3]] == getPowerSet(n, k)
-
-def getPowerSet(n, k) :
-    if n == k :
-        return [ [n] ]
-    else :
-        # getPowerSet(3, 1)
-        # result = [ [1], [1, 2, ...], [1, 3, ...] ] 
-        # temp = [ [2], [2, 3], [3] ]
-        result = [ [k] ] # k 뒤에 아무 것도 없는 경우
-        temp = [] # k 뒤에 붙는 경우
-        
-        for i in range(k+1, n+1) : # i = k+1 ~ n
-            temp = temp + getPowerSet(n, i)
-    
-        for i in range(len(temp)) :
-            temp[i] = [k] + temp[i]
-            
-        return result + temp
 
 def main():
     n = int(input())
