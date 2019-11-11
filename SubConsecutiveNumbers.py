53de50734c6b33b9d875eb9c4ba2d5d2383aa78f
HEESEOK <heeseok.chong@gmail.com>
2019-11-12 03:22:11 +0900

    Added a solution by DP

--- SubConsecutiveNumbers.py
+++ SubConsecutiveNumbers.py
@@ -6,7 +6,6 @@ has the largest sum.
 
 https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
 
-
 입력
 첫째 줄에 n개의 숫자가 주어진다. (1≤n≤100,000)
 
@@ -18,18 +17,17 @@ n개의 숫자에 대하여 연속 부분 최대합을 출력한다.
 -3 -3 -2 -4 -5 -2 -3 -4
 출력 예시
 15
-
 '''
 
 import math
 
 # Using Brute-force. O(n x n x n)
-def getMaxSubArraySum1(numbers) : 
+def getLSCSByBF(numbers):
     maxSum = -math.inf
     length = len(numbers)
 
-    for i in range(0, length) :
-        for j in range(i, length) :
+    for i in range(0, length):
+        for j in range(i, length):
             '''
             All cases n x (n-1)/2
             O(n) == sum for each case.
@@ -46,26 +44,26 @@ Using Divide and Conquer like merge sort. O(nlog n)
 Algorithm I 3. Divide and Conquer II.mp4
 get max sub sum in consecutive numbers.
 '''
-def getMaxSubArraySum2(numbers) :
+def getLSCSByDC(numbers):
     length = len(numbers)
-    if length == 1 :
+    if length == 1:
         return numbers[0]
-    
+
     mid = length // 2
     # Largest Sum Contiguous Subarray
-    LSCSInLeft = getMaxSubArraySum2(numbers[:mid])
-    LSCSInRight = getMaxSubArraySum2(numbers[mid:]) 
+    LSCSInLeft = getLSCSByDC(numbers[:mid])
+    LSCSInRight = getLSCSByDC(numbers[mid:])
     LSCSInMiddle = 0
-    leftMaxSum = 0 
+    leftMaxSum = 0
     rightMaxSum = 0
 
     tempSum = 0
-    for i in range(mid-1, -1, -1) :
+    for i in range(mid-1, -1, -1):
         tempSum += numbers[i]
         leftMaxSum = max(leftMaxSum, tempSum)
 
     tempSum = 0
-    for i in range(mid, length) :
+    for i in range(mid, length):
         tempSum += numbers[i]
         rightMaxSum = max(rightMaxSum, tempSum)
 
@@ -75,28 +73,45 @@ def getMaxSubArraySum2(numbers) :
 
 
 # Kadane’s Algorithm
-def getMaxSubArraySum3(numbers): 
+def getLSCSByKadane(numbers):
     max_so_far = 0
     max_ending_here = 0
     length = len(numbers)
 
-    for i in range(0, length): 
-        max_ending_here += numbers[i] 
-        if (max_so_far < max_ending_here): 
-            max_so_far = max_ending_here 
-  
-        if max_ending_here < 0: 
-            max_ending_here = 0   
+    for i in range(0, length):
+        max_ending_here += numbers[i]
+        if (max_so_far < max_ending_here):
+            max_so_far = max_ending_here
 
-    return max_so_far 
+        if max_ending_here < 0:
+            max_ending_here = 0
+
+    return max_so_far
+
+
+'''
+Dynamic Programming
+T(i) i(index)번째 숫자를 오른쪽 끝으로 하는 연속 부분 최대합
+T(i) = max(T(i-1) + numbers[i], numbers[i])
+'''
+def getLSCSByDP(numbers):
+    n = len(numbers)
+    Table = [0 for i in range(n)]
+    Table[0] = numbers[0]
+
+    for i in range(1, n):
+        Table[i] = max(Table[i-1] + numbers[i], numbers[i])
+
+    return max(Table)
 
 
 def main():
     numbers = [int(x) for x in input().split()]
-    print(getMaxSubArraySum1(numbers))
-    print(getMaxSubArraySum2(numbers))
-    print(getMaxSubArraySum3(numbers))
-    
+    print(getLSCSByBF(numbers))
+    print(getLSCSByDC(numbers))
+    print(getLSCSByKadane(numbers))
+    print(getLSCSByDP(numbers))
+
 
 if __name__ == "__main__":
     main()
