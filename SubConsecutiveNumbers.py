3da068b6f235cc4779534f1a6f9c3fe78c8e7edd
HEESEOK <heeseok.chong@gmail.com>
2019-11-12 05:13:47 +0900

    added a lining problem

--- SubConsecutiveNumbers.py
+++ SubConsecutiveNumbers.py
@@ -6,22 +6,41 @@ has the largest sum.
 
 https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
 
-입력
+
+n개의 숫자가 주어질 때, 연속 부분을 선택하여 그 합을 최대화 
+하는 프로그램을 작성하시오. 
+예를 들어, 다음과 같이 8개의 숫자가 있다고 하자.
+
+1 2 -4 5 3 -2 9 -10
+
+이 때, 연속 부분이란 연속하여 숫자를 선택하는 것을 말한다. 
+가능한 연속 부분으로써 [1, 2, -4], [5, 3, -2, 9], [9, -10] 등이 
+있을 수 있다. 이 연속 부분들 중에서 가장 합이 큰 연속 부분은 
+[5, 3, -2, 9] 이며, 이보다 더 합을 크게 할 수는 없다. 
+따라서 연속 부분 최대합은 5+3+(-2)+9 = 15 이다.
+
+
+입력 조건
 첫째 줄에 n개의 숫자가 주어진다. (1≤n≤100,000)
 
-출력
+출력 조건
 n개의 숫자에 대하여 연속 부분 최대합을 출력한다.
 
 입력 예시
 1 2 -4 5 3 -2 9 -10
--3 -3 -2 -4 -5 -2 -3 -4
+
 출력 예시
 15
 '''
 
 import math
 
-# Using Brute-force. O(n x n x n)
+
+'''
+Using Brute-force. O(n x n x n)
+'''
+
+
 def getLSCSByBF(numbers):
     maxSum = -math.inf
     length = len(numbers)
@@ -44,6 +63,8 @@ Using Divide and Conquer like merge sort. O(nlog n)
 Algorithm I 3. Divide and Conquer II.mp4
 get max sub sum in consecutive numbers.
 '''
+
+
 def getLSCSByDC(numbers):
     length = len(numbers)
     if length == 1:
@@ -72,7 +93,11 @@ def getLSCSByDC(numbers):
     return max(LSCSInLeft, LSCSInMiddle, LSCSInRight)
 
 
-# Kadane’s Algorithm
+'''
+Kadane’s Algorithm
+'''
+
+
 def getLSCSByKadane(numbers):
     max_so_far = 0
     max_ending_here = 0
@@ -94,6 +119,8 @@ Dynamic Programming
 T(i) i(index)번째 숫자를 오른쪽 끝으로 하는 연속 부분 최대합
 T(i) = max(T(i-1) + numbers[i], numbers[i])
 '''
+
+
 def getLSCSByDP(numbers):
     n = len(numbers)
     Table = [0 for i in range(n)]
