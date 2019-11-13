85e9b64f1314e24cd14f89b484fc6691377674fa
HEESEOK <heeseok.chong@gmail.com>
2019-11-13 18:28:25 +0900

    added comments

--- LSCS.py
+++ LSCS.py
@@ -1,5 +1,8 @@
 '''
+Dynamic Programming
+
 Largest Sum Contiguous Subarray
+
 Write an efficient program to find the sum of contiguous 
 subarray within a one-dimensional array of numbers which 
 has the largest sum.
