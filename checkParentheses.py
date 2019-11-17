c90090652e2fd08d2c1886d3d61a6d6200cc0c8e
HEESEOK <heeseok.chong@gmail.com>
2019-11-17 22:57:25 +0900

    + Time complexity

--- checkParentheses.py
+++ checkParentheses.py
@@ -5,22 +5,24 @@ The program return NO if the parentheses are like ‘
 (()))’ or ‘(()()(’.
 '''
 
-def checkParen(parentheses):    
+
+def checkParen(parentheses):
     '''
     0. return YES if the length of the parentheses is 0 or "()".
     1. find an adjacent rounded brakets like '()' in the parentheses.
     2. remove them in the parentheses.
     3. go to step #0
+    𝐎(n²)
     '''
-    
+
     if len(parentheses) <= 1:
         if len(parentheses) == 0:
             return "YES"
-        else: #parentheses include '(' or ')'.
+        else:  # parentheses include '(' or ')'.
             return "NO"
     elif parentheses == "()":
         return "YES"
-    
+
     '''    
     find '()'  
     '        i '
@@ -30,12 +32,14 @@ def checkParen(parentheses):
         if parentheses[i] == '(' and parentheses[i+1] == ')':
             newParentheses = parentheses[:i] + parentheses[i+2:]
             return checkParen(newParentheses)
-    
+
     return "NO"
+
 
 def main():
     x = input()
     print(checkParen(x))
+
 
 if __name__ == "__main__":
     main()
--- checkParenthesesByStack.py
+++ checkParenthesesByStack.py
@@ -1,73 +1,77 @@
-CONST_STACK_MAX = 100
+STACK_MAX = 100
+
+
 class Stack:
-    def __init__(self, max) :
+    def __init__(self, max):
         self.data = []
         self.maxSize = max
 
-    def push(self, n) :
-        if self.isFull() == False :
+    def push(self, n):
+        if self.isFull() == False:
             self.data.append(n)
             return True
-        else :
+        else:
             print("Your stack is full. hence, you can't push.")
             return False
 
-    def pop(self) :
-        if self.isEmpty() == True :
+    def pop(self):
+        if self.isEmpty() == True:
             print("Your stack is empty. hence, you can't pop from the stack.")
             return False
-        else :
-            # self.data = self.data[:-1]
+        else:
             self.data.pop()
             return True
 
-    def getSize(self) :
+    def getSize(self):
         return len(self.data)
 
-    def isEmpty(self) :
-        if self.getSize() == 0 :
+    def isEmpty(self):
+        if self.getSize() == 0:
             print('The stack is empty.')
             return True
-        else :
+        else:
             return False
 
-    def isFull(self) :
-        if self.getSize() == self.maxSize :
+    def isFull(self):
+        if self.getSize() == self.maxSize:
             print('The stack is full.')
             return True
-        else :
+        else:
             return False
-            
-    def getTop(self) :
-        if self.isEmpty() == True :
+
+    def getTop(self):
+        if self.isEmpty() == True:
             return -1
-        else :
+        else:
             return self.data[-1]
-            
-def checkParen(parentheses) :
+
+
+def checkParen(parentheses):
+    # 𝐎(n)
     # parentheses = "((()())())"
 
-    myStack = Stack(CONST_STACK_MAX)
-    
-    for x in parentheses :
-        if x == '(' :
-            if myStack.push(x) == False :
+    myStack = Stack(STACK_MAX)
+
+    for x in parentheses:
+        if x == '(':
+            if myStack.push(x) == False:
                 break
-        else :
-            if myStack.isEmpty() == True :
+        else:
+            if myStack.isEmpty() == True:
                 return "NO"
-            else :
+            else:
                 myStack.pop()
 
-    if myStack.isEmpty() == True :
+    if myStack.isEmpty() == True:
         return "YES"
-    else :
+    else:
         return "NO"
 
 
 def main():
     parentheses = input()
     print(checkParen(parentheses))
+
 
 if __name__ == "__main__":
     main()
