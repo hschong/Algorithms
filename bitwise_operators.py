aab52c73eec286f4f8e948a7cda809fd44c688ce
HEESEOK <heeseok.chong@gmail.com>
2020-04-30 19:51:03 +0900

    + comments for add()

--- bitwise_operators.py
+++ bitwise_operators.py
@@ -1,26 +1,29 @@
 '''
 Add two numbers using bitwise operators only.
-
-      Sub sum(^)    Carry(&)    (Carry << 1) ^ Sub sum  Total sum
-0 0   0             0           (00) ^ 00               00
-0 1   1             0           (00) ^ 01               01    
-1 0   1             0           (00) ^ 01               01
-1 1   0             1           (10) ^ 00               10
-
+Write a function Add() that returns sum of two integers. The function should not use any of the arithmetic operators (+, ++, –, -, .. etc).
+   
+x y   (x ^ y)    (x & y) << 1)      (x ^ y) ^ (x & y) << 1)
+0 0   0             0               0
+0 1   1             0               1    
+1 0   1             0               1
+1 1   0             10              10
 '''
 
 
-def add(x, y):  # x -> sub sum, y -> carry
+def add_using_while(x, y):
+    while y != 0:               # loops until no carry
+        carry = (x & y) << 1        # find the carry when adding x to y
+        xor_sum = x ^ y             # sum of x and y without carry
+        x = xor_sum
+        y = carry
+
+    return x
+
+
+def add(x, y):
     if y != 0:
+        # add(sum of x and y without carry, carry)
         add(x ^ y, (x & y) << 1)
 
-    return x  # x = sum sum ^ carry
+    return x
 
-
-def add_simple(x, y):
-    while y != 0:
-        carry = x & y  # carry
-        x = x ^ y   # sub sum
-        y = carry << 1
-
-    return x  # x = sum sum ^ carry
