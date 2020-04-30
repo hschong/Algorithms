9dc1ab6ef64368b9ba233081dbec0f6dd3782d24
HEESEOK <heeseok.chong@gmail.com>
2020-04-30 15:51:17 +0900

    + add_simple() using while loop.

--- bitwise_operators.py
+++ bitwise_operators.py
@@ -14,4 +14,13 @@ def add(x, y):  # x -> sub sum, y -> carry
     if y != 0:
         add(x ^ y, (x & y) << 1)
 
-    return x
+    return x  # x = sum sum ^ carry
+
+
+def add_simple(x, y):
+    while y != 0:
+        carry = x & y  # carry
+        x = x ^ y   # sub sum
+        y = carry << 1
+
+    return x  # x = sum sum ^ carry
