36cd97d8962db731f73b9d37784958644e762fb8
HEESEOK <heeseok.chong@gmail.com>
2020-05-13 12:41:24 +0900

    + reverse string using for loop

--- useful_examples.py
+++ useful_examples.py
@@ -46,14 +46,15 @@ else:
     print(mylist.index('a'))
 
 
-def reverse_string(x):
-    return x[-1::-1]
-    # return x[::-1]
+def reverse_string(string):
+    return string[-1::-1]  # return string[::-1]
 
-    # reversed_string = ''
-    # for i in range(len(x)-1, -1, -1):
-    #     reversed_string += x[i]
-    # return reversed_string
+
+def reverse_string_using_loop(string):
+    reversed_string = ''
+    for i in range(len(string)-1, -1, -1):
+        reversed_string += string[i]
+    return reversed_string
 
 
 mytxt = reverse_string("I wonder how this text looks like backwards")
