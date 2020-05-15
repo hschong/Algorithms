8304e2b7e971cf6375a9c8db6366646f46062f8c
HEESEOK <heeseok.chong@gmail.com>
2020-05-15 18:46:14 +0900

    + make a list into a string

--- useful_examples.py
+++ useful_examples.py
@@ -1,49 +1,64 @@
 import math
 
-my_list = [1, 2, 5, 4, 7]
+integer_list = [1, 2, 5, 4, 7]
 
 # Sort the items of the list in place.
-my_list.sort()
+integer_list.sort()
 
 # Return a new sorted list from the items in iterable.
-new_list = sorted(my_list, reverse=False)
+new_integer_list = sorted(integer_list, reverse=False)
 print(sorted([1, 3, 2, 5, 6, 4]))
 
 # Reverse a list
-new_list.reverse()
-reversed_list = list(reversed(new_list))
+new_integer_list.reverse()
+reversed_list = list(reversed(new_integer_list))
+
+# Split a string into a list where each word is a list item:
+# setting the maxsplit parameter to 3, will return a list with 4 elements!
+txt = "apple#banana#cherry#orange"
+x = txt.split("#", 3)
+print(x)
+
+# make a string into a list
+src_str = 'abc'
+print(list(src_str))
+
+# make a list into a string
+# convert a list of integers into a string list
+string_list = [str(integer) for integer in integer_list]
+print(''.join(string_list))
 
 # Remove Duplicates From a List.
-my_list = ["a", "b", "a", "c", "c"]
-my_list = list(dict.fromkeys(my_list))
-print(my_list)
+integer_list = ["a", "b", "a", "c", "c"]
+integer_list = list(dict.fromkeys(integer_list))
+print(integer_list)
 
 # add/remove an object in a list.
-my_list.append('1')
-my_list.remove('1')
-my_list.reverse()
+integer_list.append('1')
+integer_list.remove('1')
+integer_list.reverse()
 
 # remove all elements in list.
-my_list.clear()  # Python 3.3+
-del my_list[:]
-my_list[:] = []
-my_list *= 0
+integer_list.clear()  # Python 3.3+
+del integer_list[:]
+integer_list[:] = []
+integer_list *= 0
 
 # for in loop
-for item in my_list:
+for item in integer_list:
     print(item)
 
-for index in range(len(my_list)):
-    print(my_list[index])
+for index in range(len(integer_list)):
+    print(integer_list[index])
 
-for idx, val in enumerate(my_list):
+for idx, val in enumerate(integer_list):
     print(idx, val)
 
 # get the index of a given number
-if 'a' not in my_list:
+if 'a' not in integer_list:
     print(-1)
 else:
-    print(my_list.index('a'))
+    print(integer_list.index('a'))
 
 
 # Using infinity
