769756eb795d4dcfea0175f63fb2a27317c20447
HEESEOK <heeseok.chong@gmail.com>
2020-05-20 18:00:21 +0900

    O(log n) for using divide and conquer

--- power.py
+++ power.py
@@ -1,4 +1,4 @@
-def power(base, expo):
+def power_using_loop(base, expo):
     result = 1
 
     if expo == 0:
@@ -23,35 +23,11 @@ def power_using_recursion(base, expo):
     return power_using_recursion(base, expo - 1) * base if expo > 0 else power_using_recursion(base, expo + 1) / base
 
 
-print(power(2, 10))
-print(power(2, -3))
-print(power_using_recursion(2, 10))
-print(power_using_recursion(2, -3))
-
-
-'''
-만약 getPower 함수의 반환 값이 1,000,000,007 보다 클 
-경우, 1,000,000,007로 나눈 나머지 값을 반환하세요.
-
-입력
-m과 n이 주어집니다. (1 ≤ n ≤ 1,000,000,000,000)
-
-출력
-m의n승 을 1,000,000,007으로 나눈 나머지를 출력합니다.
-
-입력 예시
-3 4
-
-출력 예시
-81
-'''
-LIMIT_NUMBER = 1000000007
-
-
 def power_using_divide_and_conquer(base, expo):
     if expo == 0:
         return 1
 
+    # Time Complexity of optimized solution: O(log n)
     temp = power_using_divide_and_conquer(base, int(expo/2))
     if expo % 2 == 0:
         return temp * temp
@@ -59,4 +35,8 @@ def power_using_divide_and_conquer(base, expo):
         return temp * temp * base if expo > 0 else temp * temp / base
 
 
+print(power_using_loop(2, 10))
+print(power_using_loop(2, -3))
+print(power_using_recursion(2, 10))
+print(power_using_recursion(2, -3))
 print(power_using_divide_and_conquer(2, -3))
